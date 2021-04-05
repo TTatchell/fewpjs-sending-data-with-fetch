bdd7c1cc088b86f47f0d3baa3eb89a1d0b7600d4
TTatchell <tim.tatchell24@gmail.com>
2021-04-06 06:51:26 +1000

    not done, just uploading work on it

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    } )
+    .then(resp => {return resp.json()})
+    .then(json => {
+        console.log(json)
+        postID(json.id)})
+    
+}
+
+function postID (msg) {
+    let idMsg = document.getElementById('heading')
+    
+    idMsg.innerHTML = msg;
+
+
+}
+
+//submitData("Tim", "Tims email")
